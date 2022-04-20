00d844622c5db9f764ff71d244df34374530d543
Dao Pham <daopm.dean@gmail.com>
2022-04-20 16:35:16 +0700

    product detail

--- lib/screen/product_detail_screen.dart
+++ lib/screen/product_detail_screen.dart
@@ -17,8 +17,41 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(prod.title),
       ),
-      body: Container(
-        child: Text(prod.description),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                prod.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${prod.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: Text(
+                prod.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
