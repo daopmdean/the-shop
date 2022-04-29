8640e19e1a1d195def7ed0a3baee7fed6dda9090
Dao Pham <daopm.dean@gmail.com>
2022-04-29 15:06:15 +0700

    animation builder

--- lib/widget/auth_card.dart
+++ lib/widget/auth_card.dart
@@ -61,11 +61,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, child) => Container(
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: child,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
